485c59b7effb4d2bc3862cc4bc2779ee980e5b55
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-25 16:23:58 +0100

    add model folder

---
+++ Medichost/src/Models/IPatientsModel.ts
@@ -0,0 +1,10 @@
+export interface IPatientsModel{
+    gender: string,
+    birthday: string,
+    phoneNumber: string,
+    streetAddress: string,
+    city: string,
+    zipCode: string,
+    memberStatus: string,
+    registeredDate:string
+}
---
+++ Medichost/src/Models/IUpcommingAppoinment.ts
@@ -0,0 +1,7 @@
+export interface IUpcommingAppoinment{
+    date:string,
+    time:string,
+    treatment:string,
+    dentist:string,
+    name:string
+}
